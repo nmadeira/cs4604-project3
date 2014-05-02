ec400bb93a46e620652f268b9f41a7c7a29977df
Behrooz <behroozk@users.noreply.github.com>
2014-05-02 15:21:58 -0400

    Update index.php
    Attaching the UI components.

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php
-echo "test!";
-echo "test2!";
-?>
+<html>
+<head>
+	<title>SQLKillers Homepage</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body>
+<div class="jumbotron">
+  <h1>SQLKillers
+  <p>...</p>
+  <p>Hello World!</p>
+</div>
+</html>
