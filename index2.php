ee4c8e869d75c84cf8e0a60713dedca38c4e291b
Behrooz <behroozk@users.noreply.github.com>
2014-05-05 06:33:30 -0400

    Update index2.php
    Added menu and some formatting

--- index2.php
+++ index2.php
@@ -5,27 +5,60 @@
 	<link rel="stylesheet" type="text/css" href="css/custom.css">
 </head>
 <body>
+	<nav class="navbar navbar-default navbar-inverse navbar-fixed-top" role="navigation">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#pagetop">SQLKillers</a>
+			</div>
+
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#predtabl">Relations</a></li>
+					<li><a href="#predquer">Preset Queries</a></li>
+					<li><a href="#adhoc">Ad-hoc Queries</a></li>
+					<li><a href="#">Insertion</a></li>
+					<li><a href="#">Modification</a></li>
+					<li><a href="#">Deletion</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#">About Us</a></li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div><!-- /.container-fluid -->
+	</nav>
+	<a id="pagetop"></a>
 	<header>
-      <h1 class="reflected">SQLKillers</h1><br>
-	  <h3 class="reflected">Behrooz Kamali, Nick Madeira, Paul Sharma</h3>
-    </header>
+		<br><br><br>
+		<h1 class="reflected">SQLKillers</h1><br>
+		<a id="predtabl"></a>
+		<h3 class="reflected">Behrooz Kamali, Nick Madeira, Paul Sharma</h3>
+	</header>
 	
 	<div class="row">
 		<div class="col-xs-1"></div>
 		<div class="col-xs-10">
 	<!-- List of tables -->
+	<h3>Table Queries</h3>
 	<p>Click on any of the following relations to see the data in that relation: </p>
-	<ul id="relationList">
-		<li class="clickable bg-danger">Video</li>
-		<li class="clickable bg-danger">Performer</li>
-		<li class="clickable bg-danger">UserInfo</li>
-		<li class="clickable bg-danger">Director</li>
-		<li class="clickable bg-danger">MovieInfo</li>
-		<li class="clickable bg-danger">TvEpisodeInfo</li>
-		<li class="clickable bg-danger">Certificates</li>
-	</ul>
+	<div class="list-group" id="relationList" style="width:200px;">
+		<a href="#" class="list-group-item">Video</a>
+		<a href="#" class="list-group-item">Performer</a>
+		<a href="#" class="list-group-item">UserInfo</a>
+		<a href="#" class="list-group-item">Director</a>
+		<a href="#" class="list-group-item">MovieInfo</a>
+		<a class="interlink" id="predquer"></a>
+		<a href="#" class="list-group-item">TvEpisodeInfo</a>
+		<a href="#" class="list-group-item">Certificates</a>
+	</div>
 
 	<!-- List of queries -->
+	<h3>Preset Queries</h3>
 	<p>Click on any of the following queries to see the returned rows: </p>
 	<ul id="queryList">
 	<li>Getting list of 10 users with most friends:<br>
@@ -43,15 +76,18 @@
 			WHERE v.vid = b.vid AND v.release_year = 2001 AND genre = 'Sci-Fi' LIMIT 10;</p></li>
 	<li>Getting all users named Matt:<br>
 		<p class="clickable bg-danger">SELECT first_name,last_name FROM userinfo WHERE first_name='Matt' LIMIT 20;</p></li>
+	<a id="adhoc"></a>
 	<li>Getting all the movies that were produced by TV UNAM:<br>
 		<p class="clickable bg-danger">SELECT title FROM videoinfo WHERE producer='TV UNAM' LIMIT 20;</p></li>	
 	</ul>
 
 	<!-- Ad-hoc query form -->
+	<h3>Ad-hoc Queries</h3>
 	<form role="form" action="#">
 		<div class="form-group">
 			<label for="adhocquery">Ad-hoc query: </label>
-			<input type="text" class="form-control" id="adhocquery" placeholder="Enter query here">
+			<textarea class="form-control" id="adhocquery" placeholder="Enter query here" rows="6" style="width:75%;"></textarea>
+			<!-- <input type="text" class="form-control" id="adhocquery" placeholder="Enter query here"> -->
 		</div>
 		<button type="submit" class="btn btn-default">Submit</button>
 		<button type="reset" class="btn btn-default">Clear</button>
